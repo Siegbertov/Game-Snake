ff791367f9f24619615465bc0df5ab063a2ce01b
Vladyslav Ziberov <siegbertov@gmail.com>
2020-01-13 20:10:19 +0200

    Created Snake Class

--- snake.py
+++ snake.py
@@ -10,3 +10,23 @@ clock = pygame.time.Clock()
 class Cube(object):
     def __init__(self, position):
         self.position = position
+
+        
+class Snake(object):
+    body = []
+    stomach = []
+    UP = "UP"
+    DOWN = "DOWN"
+    RIGHT = "RIGHT"
+    LEFT = "LEFT"
+
+    def __init__(self, head_r, head_c, head_direction=RIGHT):
+        self.body.append(Cube([head_r, head_c]))
+
+        self.head_direction = head_direction
+
+    def snake_position(self):
+        all_snake_pos = []
+        for el in self.body:
+            all_snake_pos.append(el.position)
+        return all_snake_pos
